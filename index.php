6e2632d75c909898c14f659bc767bb48c0aa17fb
TheXtroyer1221 <jaddou2005@gmail.com>
2019-07-03 14:01:51 -0700

    checking the files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="login.css">
+    <title>php text</title>
+  </head>
+  <body>
+    <?php
+    $x = 5;
+     echo '<span>the number is' . " " . "$x" '</span>';
+
+     echo "<p>" the number is $x '</p>'";
+     ?>
+
+  </body>
+</html>
